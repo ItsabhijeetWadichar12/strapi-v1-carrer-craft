83cf59be3c8d2d01c38c0aad8193de5f6079567f
ItsabhijeetWadichar12 <wabhijeet378@gmail.com>
2024-10-31 12:26:51 +0530

    changes

---
+++ types/generated/components.d.ts
@@ -0,0 +1,57 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    description: '';
+    displayName: 'Education';
+    icon: 'crown';
+  };
+  attributes: {
+    degree: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    endDate: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    universityName: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+  };
+  attributes: {
+    city: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    workSummery: Schema.Attribute.Text;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    description: '';
+    displayName: 'skills';
+    icon: 'connector';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
